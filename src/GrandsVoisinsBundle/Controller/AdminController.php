3d322183eddd86505e719fa1a4a17ea88df5c07b
Romain WEEGER <romain.weeger@gmail.com>
2017-02-09 16:01:30 +0100

    Build main pages

--- src/GrandsVoisinsBundle/Controller/AdminController.php
+++ src/GrandsVoisinsBundle/Controller/AdminController.php
@@ -6,18 +6,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
-    public function showAction()
+    public function homeAction()
     {
-        return $this->render('GrandsVoisinsBundle:Admin:show.html.twig', array(
-            // ...
-        ));
+        return $this->render(
+          'GrandsVoisinsBundle:Admin:home.html.twig',
+          array(// ...
+          )
+        );
+    }
+
+    public function profileAction()
+    {
+        return $this->render(
+          'GrandsVoisinsBundle:Admin:profile.html.twig',
+          array(// ...
+          )
+        );
+    }
+
+    public function organisationAction()
+    {
+        return $this->render(
+          'GrandsVoisinsBundle:Admin:organisation.html.twig',
+          array(// ...
+          )
+        );
+    }
+
+    public function inviteAction()
+    {
+        return $this->render(
+          'GrandsVoisinsBundle:Admin:invite.html.twig',
+          array(// ...
+          )
+        );
     }
 
     public function testAction()
     {
-        return $this->render('GrandsVoisinsBundle:Admin:test.html.twig', array(
-            // ...
-        ));
+        return $this->render(
+          'GrandsVoisinsBundle:Admin:test.html.twig',
+          array(// ...
+          )
+        );
     }
 
 }
