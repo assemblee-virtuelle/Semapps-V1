0679d90795d23733afb7a5b56118e740c8040aae
Romain WEEGER <romain.weeger@gmail.com>
2017-02-16 17:44:21 +0100

    Search event binded

--- web/front/src/gv-carto/gv-carto.js
+++ web/front/src/gv-carto/gv-carto.js
@@ -10,6 +10,8 @@ Polymer({
 
     window.gvc = this;
 
+    this.$window = $(window);
+
     // Special class for dev env.
     if (window.location.hostname === '127.0.0.1') {
       window.document.body.classList.add('dev-env');
@@ -43,9 +45,52 @@ Polymer({
     }
   },
 
-  start(sources) {
+  start(data) {
     "use strict";
+    this.buildings = data.buildings;
+    // Shortcuts.
+    this.domSearchTextInput = this.domId('searchText');
+    // Listeners.
+    var callbackSearchEvent = this.searchEvent.bind(this);
+    // Click on submit button.
+    this.listen('searchForm', 'submit', (e) => {
+      this.scrollToSearch();
+      callbackSearchEvent(e);
+    });
+    // Type in search field.
+    this.listen('searchText', 'keyup', callbackSearchEvent);
+  },
 
-    log(sources);
+  scrollToSearch() {
+    this.$window.scrollTo('#searchTabs', {
+      duration: 1000,
+      easing: 'easeOutQuad'
+    });
+  },
+  searchEvent(e) {
+    // Event may be missing.
+    e && e.preventDefault();
+    var term = this.domSearchTextInput.value;
+    log(term);
+  },
+
+  listen(id, event, callback) {
+    // Support list of events names.
+    if (Array.isArray(event)) {
+      for (let i in event) {
+        this.listen(id, event[i], callback);
+      }
+      return;
+    }
+    return this.domId(id).addEventListener(event, callback);
+  },
+
+  domId(id) {
+    return document.getElementById(id);
+  },
+
+  dom(selector) {
+    return document.querySelectorAll(selector);
   }
+
 });
