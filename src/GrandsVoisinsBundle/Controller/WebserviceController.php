6c324f16ea38979ebfd0471e08bf509f1236443b
Romain WEEGER <romain.weeger@gmail.com>
2017-02-19 16:16:58 +0100

    Manage timeout

--- src/GrandsVoisinsBundle/Controller/WebserviceController.php
+++ src/GrandsVoisinsBundle/Controller/WebserviceController.php
@@ -8,7 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class WebserviceController extends Controller
 {
-    var $semanticFormsBaseUrl = 'http://semantic-forms.cc:9112/lookup';
+    // Avoid DNS lookup for domain, use direct IP.
+    var $semanticFormsBaseUrl = 'http://163.172.179.125:9112/lookup';
 
     public function buildingAction()
     {
@@ -38,7 +39,7 @@ class WebserviceController extends Controller
     {
         $term = $request->query->get('t');
         // Build a fake empty response in case of fail.
-        $response = (object)['results' => []];
+        $output = (object)['results' => []];
 
         if ($term) {
             $curl = curl_init();
@@ -49,10 +50,17 @@ class WebserviceController extends Controller
             );
             curl_setopt($curl, CURLOPT_HEADER, false);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
             $response = json_decode(curl_exec($curl));
+            // No error happened.
+            if (!curl_errno($curl)) {
+                $output = $response;
+            } else {
+                $output->error = 'TIMEOUT';
+            }
             curl_close($curl);
         }
-
-        return new JsonResponse($response);
+        return new JsonResponse($output);
     }
 }
