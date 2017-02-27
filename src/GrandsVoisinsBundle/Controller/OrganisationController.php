2b85f1562b517f7239c3ff3b9e3f3d3c7f0667c3
Romain WEEGER <romain.weeger@gmail.com>
2017-02-27 21:35:23 +0100

    Prepare settings form

--- src/GrandsVoisinsBundle/Controller/OrganisationController.php
+++ src/GrandsVoisinsBundle/Controller/OrganisationController.php
@@ -4,6 +4,7 @@ namespace GrandsVoisinsBundle\Controller;
 
 use GrandsVoisinsBundle\Entity\Organisation;
 use GrandsVoisinsBundle\Entity\User;
+use GrandsVoisinsBundle\Form\AdminSettings;
 use GrandsVoisinsBundle\Form\OrganisationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,12 +14,17 @@ class OrganisationController extends Controller
     public function allAction(Request $request)
     {
 
-        $organisationEntity = $this->getDoctrine()->getManager()->getRepository('GrandsVoisinsBundle:Organisation');
-        $organisations = $organisationEntity->findAll();
+        $organisationEntity = $this->getDoctrine()->getManager()->getRepository(
+          'GrandsVoisinsBundle:Organisation'
+        );
+        $organisations      = $organisationEntity->findAll();
 
         //form pour l'organisation
         $organisation = new Organisation();
-        $form = $this->get('form.factory')->create(OrganisationType::class,$organisation);
+        $form         = $this->get('form.factory')->create(
+          OrganisationType::class,
+          $organisation
+        );
 
         $form->handleRequest($request);
 
@@ -42,11 +48,11 @@ class OrganisationController extends Controller
 
             // Generate password.
             $tokenGenerator = $this->container->get(
-                'fos_user.util.token_generator'
+              'fos_user.util.token_generator'
             );
             $randomPassword = substr($tokenGenerator->generateToken(), 0, 12);
             $user->setPassword(
-                password_hash($randomPassword, PASSWORD_BCRYPT, ['cost' => 13])
+              password_hash($randomPassword, PASSWORD_BCRYPT, ['cost' => 13])
             );
 
             $user->setSfUser($randomPassword);
@@ -66,31 +72,37 @@ class OrganisationController extends Controller
             $em->flush();
 
             // send email to th new organization
-            $body=  "Bonjour ".$user->getUsername()." !<br><br>
+            $body = "Bonjour ".$user->getUsername()." !<br><br>
                     Pour valider votre compte utilisateur, merci de vous rendre sur http://localhost:8000/register/confirm/".$conf_token.".<br><br>
                     Ce lien ne peut être utilisé qu'une seule fois pour valider votre compte.<br><br>
                     Nom de compte : ".$user->getUsername()."<br>
                     Mot de passe : ".$randomPassword."<br><br>
                     Cordialement,
                     L'équipe";
-            $this->get('GrandsVoisinsBundle.EventListener.SendMail')->sendConfirmMessage($user, $body);
+            $this->get('GrandsVoisinsBundle.EventListener.SendMail')
+              ->sendConfirmMessage($user, $body);
 
             // TODO Grant permission to edit same organisation as current user.
             // Display message.
             $this->addFlash(
-                'success',
-                'Un compte à bien été créé pour <b>' .
-                $user->getUsername() .
-                '</b>. Un email a été envoyé à <b>' .
-                $user->getEmail() .
-                '</b> pour lui communiquer ses informations de connexion.'
+              'success',
+              'Un compte à bien été créé pour <b>'.
+              $user->getUsername().
+              '</b>. Un email a été envoyé à <b>'.
+              $user->getEmail().
+              '</b> pour lui communiquer ses informations de connexion.'
             );
+
             return $this->redirectToRoute('all_orga');
         }
-        return $this->render('GrandsVoisinsBundle:Organisation:home.html.twig', array(
-            "organisations" => $organisations,
+
+        return $this->render(
+          'GrandsVoisinsBundle:Organisation:home.html.twig',
+          array(
+            "organisations"       => $organisations,
             "formAddOrganisation" => $form->createView(),
-        ));
+          )
+        );
     }
 
     public function newOrganisationAction()
@@ -98,17 +110,15 @@ class OrganisationController extends Controller
         $sfClient = $this->container->get('semantic_forms.client');
         // questionner la base pour savoir si l'orga est deja créer
         $organisationEntity = $this->getDoctrine()->getManager()->getRepository(
-            'GrandsVoisinsBundle:Organisation'
+          'GrandsVoisinsBundle:Organisation'
         );
-        $organisation = $organisationEntity->findOneById(
-            $this->GetUser()->getFkOrganisation()
+        $organisation       = $organisationEntity->findOneById(
+          $this->GetUser()->getFkOrganisation()
         );
-        if (is_null($organisation->getSfOrganisation())){
+        if (is_null($organisation->getSfOrganisation())) {
             $json = $sfClient->createFoaf('Organization');
             $edit = false;
-        }
-
-        else{
+        } else {
             $json = $sfClient->getForm($organisation->getSfOrganisation());
             $edit = true;
         }
@@ -118,11 +128,11 @@ class OrganisationController extends Controller
         }
 
         return $this->render(
-            'GrandsVoisinsBundle:Organisation:organisation.html.twig',
-            array(
-                'organisation'  => $json,
-                'edit'          => $edit
-            )
+          'GrandsVoisinsBundle:Organisation:organisation.html.twig',
+          array(
+            'organisation' => $json,
+            'edit'         => $edit,
+          )
         );
     }
 
@@ -133,43 +143,59 @@ class OrganisationController extends Controller
 
         foreach ($_POST as $key => $value) {
             unset($_POST[$key]);
-            $_POST[str_replace("_", '.',urldecode($key))] = $value;
+            $_POST[str_replace("_", '.', urldecode($key))] = $value;
         }
 
         $info = $this->container
-            ->get('semantic_forms.client')
-            ->send($_POST);
+          ->get('semantic_forms.client')
+          ->send($_POST);
 
         //TODO: a modifier pour prendre l'utilisateur courant !
         if ($info == 200) {
-            if(!$edit){
+            if (!$edit) {
                 $organisationEntity = $this->getDoctrine()
-                    ->getManager()
-                    ->getRepository('GrandsVoisinsBundle:Organisation');
-                $query              = $organisationEntity->createQueryBuilder('q')
-                    ->update()
-                    ->set('q.sfOrganisation', ':link')
-                    ->where('q.id=:id')
-                    ->setParameter('link', $_POST["uri"])
-                    ->setParameter('id', $this->getUser()->getfkOrganisation())
-                    ->getQuery();
+                  ->getManager()
+                  ->getRepository('GrandsVoisinsBundle:Organisation');
+                $query              = $organisationEntity->createQueryBuilder(
+                  'q'
+                )
+                  ->update()
+                  ->set('q.sfOrganisation', ':link')
+                  ->where('q.id=:id')
+                  ->setParameter('link', $_POST["uri"])
+                  ->setParameter('id', $this->getUser()->getfkOrganisation())
+                  ->getQuery();
                 $query->getResult();
             }
 
             $this->addFlash(
-                'success',
-                'tous est <b>ok !</b>'
+              'success',
+              'tous est <b>ok !</b>'
             );
 
             return $this->redirectToRoute('profile');
 
         } else {
             $this->addFlash(
-                'success',
-                'quelque chose est <b>nok ...</b>'
+              'success',
+              'quelque chose est <b>nok ...</b>'
             );
+
             return $this->redirectToRoute('organisation');
         }
     }
 
+    public function settingsAction()
+    {
+        $user = $this->GetUser();
+        $form = $this->get('form.factory')->create(AdminSettings::class, $user);
+
+        return $this->render(
+          'GrandsVoisinsBundle:Admin:settings.html.twig',
+          array(
+            'form' => $form->createView(),
+            'user' => $user
+          )
+        );
+    }
 }
---
+++ src/GrandsVoisinsBundle/Form/AdminSettings.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace GrandsVoisinsBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class AdminSettings extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+          'username',
+          TextType::class,
+          array(
+            'label'       => 'login',
+            'constraints' => array(
+              new NotBlank(),
+            ),
+          )
+        )
+          ->add(
+            'email',
+            EmailType::class,
+            [
+              'label' => 'Adresse email',
+            ]
+          )
+          ->add(
+            'password',
+            PasswordType::class,
+            [
+              'label' => 'Mot de passe',
+            ]
+          )
+          ->add(
+            'passwordNew',
+            PasswordType::class,
+            [
+              'label'  => 'Nouveau mot de passe',
+              'mapped' => false,
+            ]
+          )
+          ->add(
+            'passwordNewConfirm',
+            PasswordType::class,
+            [
+              'label'  => 'Confirmation',
+              'mapped' => false,
+            ]
+          )
+          ->add('submit', SubmitType::class, ['label' => 'Enregistrer']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+          array(
+            'data_class' => 'GrandsVoisinsBundle\Entity\User',
+          )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'grandsvoisinsbundle_user';
+    }
+
+
+}
