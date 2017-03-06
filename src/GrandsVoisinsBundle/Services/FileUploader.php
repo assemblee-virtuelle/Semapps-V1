a5a25bd4a7db130f2ccbdeadb9fb191f06a4d86a
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-03-06 17:38:34 +0100

    PREPARING picture upload

---
+++ src/GrandsVoisinsBundle/Services/FileUploader.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LaFaucheuse
+ * Date: 06/03/2017
+ * Time: 17:25
+ */
+
+namespace GrandsVoisinsBundle\Services;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileUploader
+{
+    private $targetDir;
+
+    public function __construct($targetDir)
+    {
+        $this->targetDir = $targetDir;
+    }
+
+    public function upload(UploadedFile $file)
+    {
+        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+        $file->move($this->targetDir, $fileName);
+
+        return $fileName;
+    }
+
+    public function getTargetDir()
+    {
+        return $this->targetDir;
+    }
+}
