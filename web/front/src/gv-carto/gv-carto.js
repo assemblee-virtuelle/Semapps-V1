0630968b06e4c0ff697d83caf3ae31aae4867872
Romain WEEGER <romain.weeger@gmail.com>
2017-03-07 11:41:08 +0100

    My account button works

--- web/front/src/gv-carto/gv-carto.js
+++ web/front/src/gv-carto/gv-carto.js
@@ -1,5 +1,18 @@
 Polymer({
   is: 'gv-carto',
+  properties: {
+    route: {
+      type: Object,
+      observer: '_routeChanged'
+    }
+  },
+
+  _routeChanged(data) {
+    "use strict";
+    if (data.path === '/mon-compte') {
+      window.location.replace(data.path);
+    }
+  },
 
   ready() {
     "use strict";
