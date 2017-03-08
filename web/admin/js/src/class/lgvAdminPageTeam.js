68617b3beba7c509fc5c9117846764c70ad8ac5c
Romain WEEGER <romain.weeger@gmail.com>
2017-03-08 14:47:44 +0100

    Manage select change on team page

--- web/admin/js/src/class/lgvAdminPageTeam.js
+++ web/admin/js/src/class/lgvAdminPageTeam.js
@@ -6,12 +6,22 @@ class LgvAdminPageTeam extends LgvAdminPage {
     $('.team-user-delete').click((e) => {
       // Disable default click behavior.
       e.preventDefault();
-      var userId = $(e.currentTarget).attr('rel');
+      let userId = $(e.currentTarget).attr('rel');
       // Use custom modal for message.
       this.admin.modalConfirm('Êtes-vous sûr de vouloir supprimer ce compte ? ' +
         'Toutes les informations du profil seront perdues, ' +
         'et le membre n\'aura plus accès au site.', () => {
         window.location.replace('/mon-compte/user/delete/' + userId);
+      });
+    });
+
+    $(document.getElementById('teamManager')).find('tr').each(function () {
+      let $this = $(this);
+      let userId = $this.attr('rel');
+      // Manage select changes.
+      $this.find('select[name=accessLevel]').change(function () {
+        // TODO Faire les contolleurs change-user-access/userId/accessLevel
+        console.log(userId);
       });
     });
   }
