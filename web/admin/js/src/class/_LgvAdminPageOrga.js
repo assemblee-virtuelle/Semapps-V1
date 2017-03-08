f3fdafb3a09ea154ae1f0245bbacc3104ecee93d
Romain WEEGER <romain.weeger@gmail.com>
2017-03-08 14:17:19 +0100

    Prepare js orga file

---
+++ web/admin/js/src/class/_LgvAdminPageOrga.js
@@ -0,0 +1,16 @@
+class LgvAdminPageOrga extends LgvAdminPage {
+  constructor(admin) {
+    super(admin);
+
+    // On user profile, remove user button.
+    $('.orga-delete').click((e) => {
+      // Disable default click behavior.
+      e.preventDefault();
+      var userId = $(e.currentTarget).attr('rel');
+      // Use custom modal for message.
+      this.admin.modalConfirm('Êtes-vous sûr de vouloir supprimer cette organisation ?', () => {
+        window.location.replace('/mon-compte/orga/delete/' + userId);
+      });
+    });
+  }
+}
