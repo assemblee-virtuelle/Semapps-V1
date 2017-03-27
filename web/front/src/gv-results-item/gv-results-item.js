0191363926383feca26b570d6a7419ff64c26f53
Romain WEEGER <romain.weeger@gmail.com>
2017-03-27 14:38:01 +0200

    Default images

--- web/front/src/gv-results-item/gv-results-item.js
+++ web/front/src/gv-results-item/gv-results-item.js
@@ -9,19 +9,17 @@ Polymer({
 
   handleClick(e) {
     e.preventDefault();
-    gvc.goToPath('detail',{
+    gvc.goToPath('detail', {
       uri: window.encodeURIComponent(this.uri)
     });
-  },
-
-  noImage() {
-    "use strict";
-    return !!this.image;
   },
 
   attached() {
     "use strict";
     $.extend(this, this.data);
+    if (!this.image) {
+      this.image = '/common/images/result-no_picture-' + gvc.searchTypes[this.type].type + '.png';
+    }
     this.info = gvc.searchTypes[this.type].label;
     if (this.subject) {
       this.info += ' | ' + this.subject;
