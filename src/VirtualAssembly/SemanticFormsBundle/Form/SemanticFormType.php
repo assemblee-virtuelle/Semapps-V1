e89e4c62a2a179a7a4ed9a91d2e5256a1fe90240
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-04-03 10:54:24 +0200

    remove debug

--- src/VirtualAssembly/SemanticFormsBundle/Form/SemanticFormType.php
+++ src/VirtualAssembly/SemanticFormsBundle/Form/SemanticFormType.php
@@ -134,7 +134,7 @@ class SemanticFormType extends AbstractType
                       $saveData[$htmlName] = $value;
                   }
               }
-              dump($saveData);
+              //dump($saveData);
               $client->send(
                 $saveData,
                 $login,
