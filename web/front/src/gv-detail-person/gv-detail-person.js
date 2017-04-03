3c7b1d5b747819d210813713f6f3b862cd2a1d6b
Romain WEEGER <romain.weeger@gmail.com>
2017-04-03 17:50:28 +0200

    Add birthday

--- web/front/src/gv-detail-person/gv-detail-person.js
+++ web/front/src/gv-detail-person/gv-detail-person.js
@@ -19,5 +19,9 @@ Polymer({
     this.topicInterest = this.data.topicInterest;
     this.expertize = this.data.expertize;
     this.knows = this.data.knows;
+    if (this.birthday) {
+      let birthday = new Date(this.birthday);
+      this.birthday = birthday.getDate() + '/' + (birthday.getMonth() + 1) + '/' + birthday.getFullYear();
+    }
   }
 });
