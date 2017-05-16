fbb2f1a09d3154757964c58d5469f8896556a94b
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-05-16 14:16:47 +0200

    adding the possibility to remove one component ( project, event, proposition ) ( adding js)

---
+++ web/admin/js/src/class/lgvAdminPageComponent.js
@@ -0,0 +1,19 @@
+class LgvAdminPageComponent extends LgvAdminPage {
+  init() {
+    super.init();
+
+    // On user profile, remove user button.
+    $('.component-delete').click((e) => {
+      // Disable default click behavior.
+      e.preventDefault();
+      let uri = $(e.currentTarget).attr('rel');
+      let nameComponent = $(e.currentTarget).attr('name');
+      // Use custom modal for message.
+      this.admin.modalConfirm('Êtes-vous sûr de vouloir supprimer ce '+nameComponent+' ? ' +
+        'Toutes les informations seront perdues. ', () => {
+        window.location.replace('/mon-compte/component/delete?uri=' + uri+'&componentName='+nameComponent);
+      });
+    });
+
+  }
+}
