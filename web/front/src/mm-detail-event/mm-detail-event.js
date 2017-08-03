1af6c8751f4e6ed026583b569b0eade29196f106
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-08-03 14:22:05 +0200

    WORK IN PROGRESS : moved to the new ontology event public part done !

--- web/front/src/mm-detail-event/mm-detail-event.js
+++ web/front/src/mm-detail-event/mm-detail-event.js
@@ -15,18 +15,18 @@ Polymer({
         });
         // Raw values.
         $.extend(this, this.data.properties);
-        this.topicInterest = this.data.topicInterest;
-        this.resourceNeeded = this.data.resourceNeeded;
-        this.resourceProposed = this.data.resourceProposed;
-        this.image = this.data.properties.image;
-        this.buildingTitle = gvc.buildings[this.data.properties.building].title;
-        if (this.eventBegin) {
-            let eventBegin = new Date(this.eventBegin);
-            this.eventBegin = eventBegin.getDate() + '/' + (eventBegin.getMonth() + 1) + '/' + eventBegin.getFullYear() + ' ' + eventBegin.getHours() + ' H ' + eventBegin.getMinutes() + ' min';
+        this.representedBy = this.data.representedBy;
+        this.hasInterest = this.data.hasInterest;
+        this.organizedBy = this.data.organizedBy;
+        this.hasParticipant = this.data.properties.hasParticipant;
+        //this.buildingTitle = gvc.buildings[this.data.properties.building].title;
+        if (this.startDate) {
+            let startDate = new Date(this.startDate);
+            this.startDate = startDate.getDate() + '/' + (startDate.getMonth() + 1) + '/' + startDate.getFullYear() + ' ' + startDate.getHours() + ' H ' + startDate.getMinutes() + ' min';
         }
-        if (this.eventEnd) {
-            let eventEnd = new Date(this.eventEnd);
-            this.eventEnd = eventEnd.getDate() + '/' + (eventEnd.getMonth() + 1) + '/' + eventEnd.getFullYear() + ' ' + eventEnd.getHours() + ' H ' + eventEnd.getMinutes() + ' min';
+        if (this.endDate) {
+            let endDate = new Date(this.endDate);
+            this.endDate = endDate.getDate() + '/' + (endDate.getMonth() + 1) + '/' + endDate.getFullYear() + ' ' + endDate.getHours() + ' H ' + endDate.getMinutes() + ' min';
         }
     },
 
