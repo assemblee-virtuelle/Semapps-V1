f28c878ea11bf08629cca9f15c7a1d08db274e45
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-08-25 13:42:08 +0200

    For issue #20 #28 #29, Adding new page : information pratique

---
+++ web/front/src/mm-infos/mm-infos.js
@@ -0,0 +1,28 @@
+Polymer({
+    is: 'mm-infos',
+    properties: {
+        queryParams: {
+            observer: '_queryChanged'
+        }
+    },
+    _queryChanged: function (data) {
+        // We are on the search mode.
+        "use strict";
+        log("programme")
+        // Wait main object to be ready.
+        GVCarto.ready(() => {
+            this.infosLoad();
+        });
+
+    },
+    infosLoad () {
+        "use strict";
+        log( gvc.myRoute)
+        if( gvc.myRoute === "infos"){
+            // Show spinner.
+            this.loading = true;
+            // Request server.
+        }
+    },
+
+});
