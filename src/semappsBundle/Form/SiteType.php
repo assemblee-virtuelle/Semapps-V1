2c59a73b7d163712a27677de6419dd962ff6b0eb
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-12-01 11:37:07 +0100

    wip : site federation -- save some website

---
+++ src/semappsBundle/Form/SiteType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace semappsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SiteType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+					->add('name', TextType::class)
+					->add('site', UrlType::class)
+					->add('submit', SubmitType::class, array());
+				;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'semappsBundle\Entity\Site'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'semappsbundle_site';
+    }
+
+
+}
