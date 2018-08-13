99e163b87ea1a03cf88ddba51b9fb717d8561faa
seblemoine <sebastien.lemoine@lilo.org>
2018-08-13 10:30:48 +0200

    wip : readding component + dynamic menu creation

---
+++ src/mainBundle/Controller/ComponentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mainBundle\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use VirtualAssembly\semappsBundle\Controller\ComponentController as BaseComponent;
+
+class ComponentController extends BaseComponent
+{
+
+}
