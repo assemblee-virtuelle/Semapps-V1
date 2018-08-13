a9d3068e2be8af9b98aa7d3603e17e6ce689077e
seblemoine <sebastien.lemoine@lilo.org>
2018-08-13 15:19:36 +0200

    wip : readding old forms + dynamic admin menu

---
+++ src/mainBundle/Form/DocumentTypeType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace semappsBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use VirtualAssembly\SemanticFormsBundle\Form\MultipleType;
+use VirtualAssembly\SemanticFormsBundle\Form\SemanticFormType;
+
+
+class DocumentTypeType extends SemanticFormType
+{
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // This will manage form specification.
+        parent::buildForm($builder, $options);
+
+        $this
+            ->add($builder, 'preferedLabel', TextType::class)
+            ->add($builder, 'alternativeLabel', TextType::class, ['required' => false,])
+            ->add(
+                $builder,
+                'description',
+                TextareaType::class,
+                [
+                    'required' => false,
+                ]
+            )
+            ->add(
+                $builder,
+                'comment',
+                TextType::class,
+                [
+                    'required' => false,
+                ]
+            )
+            ->add(
+                $builder,
+                'homePage',
+                UrlType::class,
+                [
+                    'required' => false,
+                ]
+            )
+            ->add(
+                $builder,
+                'aboutPage',
+                MultipleType::class,
+                [
+                    'required' => false,
+                ]
+            )
+            ->add(
+                $builder,
+                'hasTopConcept',
+                HiddenType::class,
+                [
+                    'required'  => false,
+                    'data'     => $options['graphURI'],
+                    'empty_data'     => $options['graphURI'],
+                ]
+            )
+        ;
+
+        $builder->add('save', SubmitType::class, ['label' => 'Enregistrer']);
+    }
+}
